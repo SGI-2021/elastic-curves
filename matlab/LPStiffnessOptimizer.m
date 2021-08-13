a18b2c921d61ca655fe2a3e0ea4a8c9db83785bb
natasha-diederen <natasha.diederen@gmail.com>
2021-08-13 13:31:39 +0100

    Refactored code

--- matlab/LPStiffnessOptimizer.m
+++ matlab/LPStiffnessOptimizer.m
@@ -7,7 +7,7 @@ classdef LPStiffnessOptimizer < handle
         kappa % a 1-by-num_samples row vector of curvature samples
         gamma_infl = zeros(2,0) % a 2-by-n matrix of inflection points; each column contains the x- and y-coordinates of an inflection point
         num_samples % the number of curve samples (and curvature samples)
-        err
+        err % error message set to 0 if there are no errors and 1 if an error has occured
     end
     
     methods
@@ -20,6 +20,8 @@ classdef LPStiffnessOptimizer < handle
                 obj.gamma_infl = gamma_infl;
             end
             obj.num_samples = length(kappa);
+
+            % Initialise error to 0
             obj.err = 0;
         end
         
@@ -29,11 +31,21 @@ classdef LPStiffnessOptimizer < handle
         % and the 2-by-1 column vector "b".
         function [K, a, b] = optimizeSimple(obj)
             f = [0,0,0,1];
-            A1 = [-ones(obj.num_samples, 1) ./ obj.kappa', -obj.gamma(1,:)' ./ obj.kappa', -obj.gamma(2,:)' ./ obj.kappa', zeros(obj.num_samples, 1)];
-            A2 = [ones(obj.num_samples, 1) ./ obj.kappa', obj.gamma(1,:)' ./ obj.kappa', obj.gamma(2,:)' ./ obj.kappa', -ones(obj.num_samples, 1)];
+            
+            % Setting the inequality matrix
+            A1 = [-ones(obj.num_samples, 1) ./ obj.kappa', ...
+                  -obj.gamma(1,:)' ./ obj.kappa', ...
+                  -obj.gamma(2,:)' ./ obj.kappa', ... 
+                  zeros(obj.num_samples, 1)];
+            A2 = [ones(obj.num_samples, 1) ./ obj.kappa', ...
+                  obj.gamma(1,:)' ./ obj.kappa', ...
+                  obj.gamma(2,:)' ./ obj.kappa', ...
+                  -ones(obj.num_samples, 1)];
             A = [A1; A2];
+
             b = [-ones(obj.num_samples, 1), zeros(obj.num_samples, 1)];
-            abM = linprog(f,A,b);
+
+            abM = linprog(f,A,b); % solving the linear programme
 
             K = -(abM(1:3)' * A(1:obj.num_samples,1:3)');
             a = abM(1);
@@ -45,39 +57,49 @@ classdef LPStiffnessOptimizer < handle
         % inflection points to the linear program explicitly.
         function [K, a, b] = optimizeWithInflections(obj)
             f = [0,0,0,1];
-            A1 = [-ones(obj.num_samples, 1) ./ obj.kappa', -obj.gamma(1,:)' ./ obj.kappa', -obj.gamma(2,:)' ./ obj.kappa', zeros(obj.num_samples, 1)];
-            A2 = [ones(obj.num_samples, 1) ./ obj.kappa', obj.gamma(1,:)' ./ obj.kappa', obj.gamma(2,:)' ./ obj.kappa', -ones(obj.num_samples, 1)];
+
+            % Setting the inequality matrix
+            A1 = [-ones(obj.num_samples, 1) ./ obj.kappa', ...
+                  -obj.gamma(1,:)' ./ obj.kappa', ... 
+                  -obj.gamma(2,:)' ./ obj.kappa', ... 
+                  zeros(obj.num_samples, 1)];
+            A2 = [ones(obj.num_samples, 1) ./ obj.kappa', ...
+                  obj.gamma(1,:)' ./ obj.kappa', ...
+                  obj.gamma(2,:)' ./ obj.kappa', ... 
+                  -ones(obj.num_samples, 1)];
             A = [A1; A2];
+
             b = [-ones(obj.num_samples, 1), zeros(obj.num_samples, 1)];
+            
+            % Setting the equality matrix
             Aeq = [ones(size(obj.gamma_infl,2),1), obj.gamma_infl(1,:)', obj.gamma_infl(2,:)', zeros(size(obj.gamma_infl,2),1)];
             beq = zeros(size(obj.gamma_infl,2),1);
-
-            if(sum(isinf(A)) == 0)
-                abM = linprog(f,A,b,Aeq,beq);
+            
+            % Checking whether there are any infinities in the matrix A and
+            % returning an error if so
+            if(sum(isinf(A)) == 0) 
+                abM = linprog(f,A,b,Aeq,beq); % solving the linear programme
                 obj.err = 0;
             else
                 obj.err = 1;
                 fprintf('Curve is infeasible \n');
                 abM = [];
-
             end
             
+            % Checking whether or not a solution exists and returning an
+            % error if not
             if(isempty(abM))
                 obj.err = 1;
                 fprintf('Curve is infeasible \n');
-                %obj.err = 1;
                 a = [];
                 b = [];
                 K = [];
-                %warning('off');
-
             else
                 K = -(abM(1:3)' * A(1:obj.num_samples,1:3)');
                 a = abM(1);
                 b = abM(2:3);
                 obj.err = 0;
             end
-
         end
     end
 end
--- matlab/UserInterface.m
+++ matlab/UserInterface.m
@@ -1,11 +1,8 @@
 classdef UserInterface < handle
-    %USERINTERFACE Summary of this class goes here
-    %   Detailed explanation goes here
-    % Method for each mouse event (click, drag, up)
-    % How to distiguish between adding new points and dragging around
-    % doc figure -> see also -> figure properties for documentations
+    % User interface which allows the user to edit a spline curve in real
+    % time and get the strip needed to form that curve
     
-    properties%(Access = private)
+    properties(Access = private)
         cp  % control points
     end
 
@@ -23,43 +20,51 @@ classdef UserInterface < handle
         spline % spline curve object
         num_samples % number of samples taken
 
-        pointpatch % 
-        mouseflag
+        pointpatch % patch graphics object for drawing the control points
+        mouseflag % set to true if the mouse is down and near a point, set to false otherwise
         selectedInd % current index
     end
     
     methods
          function obj = UserInterface(cp)
+            % Set up the figure for plotting the spline such that points
+            % can be moved around with the mouse
             obj.fig = figure('WindowButtonDownFcn', @(source, event) obj.mouseDown(source),...
                              'WindowButtonUpFcn', @(source, event) obj.mouseUp(source, event), ...
                              'WindowButtonMotionFcn', @(source, event) obj.mouseMove(source, event));
-            
 
             obj.ax = axes(obj.fig);
             title('Spline curve');
             xlim([-1,1]);
             ylim([-1,1]);
             obj.cp = cp;
-
+            
+            % Initialise mouse flag to false
             obj.mouseflag = false;
-
+            
             hold on;
+            % Plot of the control points
             obj.pointpatch = patch('Vertices', cp', ...
                                    'Faces', [1:size(cp,2)]', ...
                                    'Parent', obj.ax, ...
                                    'Marker', '.');
+            % Plot of lines between adjacent control points
             obj.lines = plot(obj.cp(1,:), obj.cp(2,:),'k--');
 
+            % Plot of the spline curve
             obj.spline = SplineCurve(3, obj.cp);
-
+            
+            % Initialise selectedInd and numSamples
             obj.selectedInd = 0;
             obj.num_samples = 400;
+
             % Sample the curve, and approximate the arc-length parameter s at every
             % sample
             t_samples = linspace(0,obj.spline.t_max, obj.num_samples);
             gamma = obj.spline.evaluate(t_samples);
             obj.splineplot = plot(gamma(1,:), gamma(2,:),'LineWidth',2,'Color',[1 0 0]);
 
+            % Plotting a second figure with the elastic strip
             obj.figstrip = figure();
             obj.axstrip = axes(obj.figstrip);
             obj.strip_patch = patch(obj.axstrip);
@@ -75,14 +80,9 @@ classdef UserInterface < handle
             % LEFT CLICK DRAG: MOVE POINT
             % SHIFT CLICK: ADD POINT
             % CONTROL CLICK : DELETE POINT
-            
-            % MOVE POINTS (LEFT CLICK):
-            
-
-            
 
             if(strcmp(source.SelectionType, 'normal')) 
-                % Checks to see if a left-click is made and updates
+                % Checks to see if a left-click is made near a point and updates
                 % the mouseflag to be used by mouseMove              
                 
                 [d, obj.selectedInd] = obj.closestPoint();
@@ -93,29 +93,38 @@ classdef UserInterface < handle
                     obj.mouseflag = false;
                 end
 
-                
-%                 currpoint = obj.ax.CurrentPoint;
-%                 coord = [currpoint(1,1); currpoint(1,2)];
-% 
-%  
             % ADDING POINTS (SHIFT CLICK):
             elseif(strcmp(source.SelectionType, 'extend')) 
                 % Checks if a shift-click is made near a control point
                 % Adds another control point in between  the control point
                 % that was shift-clicked and the next one. If the last 
-                % control point is shift-clicked, adds a control point in
-                % between the last and second from last control points             
+                % control point is shift-clicked, adds a control point 
+                % after the the previous end point
 
                 if(isempty(obj.cp))
                     % If there are no control points add a control point
-                    % where the cursors is
+                    % where the cursor is
                     currpoint = obj.ax.CurrentPoint;
                     coord = [currpoint(1,1); currpoint(1,2)];
-
+                    
+                    % Draw control point
                     obj.updateSpline(coord);
                     obj.drawPoints();
                     
                     return
+                end
+
+                if(size(obj.cp,2) == 1)
+                    % If there is only one control point, add another 
+                    % control point where the cursor is
+                    currpoint = obj.ax.CurrentPoint;
+                    coord = [currpoint(1,1); currpoint(1,2)];
+                    c = [obj.cp, coord];
+
+                    % Draw control point
+                    obj.updateSpline(c);
+                    obj.drawPoints();
+
                 end
 
                 % Computes nearest control point
@@ -124,17 +133,33 @@ classdef UserInterface < handle
                 if(d < 0.1)
                     % Adds control point
                     if(closestInd ~= size(obj.cp,2)) 
+                        % Find midpoint between selected point and next
+                        % point
                         midpoint = ((obj.cp(:,closestInd) + obj.cp(:,closestInd+1))'./2)';
+                        
+                        % Add the midpoint to the list of control points at
+                        % the index after the selected point
                         c1 = obj.cp(:,1:closestInd);
                         c2 = midpoint;
                         c3 = obj.cp(:,closestInd+1:end);
                         c = [c1 c2 c3];
+
                         obj.updateSpline(c);
+
                     elseif(closestInd == size(obj.cp,2))
+                        % Find midpoint between last point and penultimate
+                        % point
                         midpoint = ((obj.cp(:,end) + obj.cp(:,end-1))'./2)'; 
                         direction = midpoint - obj.cp(:,end-1);
+
+                        % Create a new last point that is the same distance
+                        % away from the last point as the midpoint is from
+                        % the last point
                         newlastpoint = obj.cp(:,end) + direction;
-                        obj.updateSpline([obj.cp, newlastpoint]);
+                        
+                        %Add the new last point to the control points
+                        c = [obj.cp, newlastpoint];
+                        obj.updateSpline(c);
                     end
 
                     % Draws contol point
@@ -144,8 +169,7 @@ classdef UserInterface < handle
 
                 else
                     obj.mouseflag = false;
-                end
-              
+                end 
                                
             % DELETING POINTS (CONTROL-CLICK)
             elseif(strcmp(source.SelectionType, 'alt'))
@@ -169,18 +193,19 @@ classdef UserInterface < handle
                 end
                 
             end
+            
         end
 
-     
         % Updates mouseflag when mouse is not being pressed
         function mouseUp(obj, source, event)
+            % Set mouseflag to false since mouse is up
             obj.mouseflag = false;
-            %obj.drawStrip();
-
+            
+            % Reset selectedInd
             obj.selectedInd = 0;
         end
 
-        % Moves a Control Point 
+        % Moves a control point 
         function mouseMove(obj, source, event)
             
             if(obj.mouseflag == true)
@@ -192,7 +217,7 @@ classdef UserInterface < handle
                  c(:, obj.selectedInd) = coords;
                  obj.updateSpline(c);
 
-                 
+                 % Draws points, spline and strip in real time
                  obj.drawPoints();
                  obj.drawSpline();
                  obj.drawStrip();
@@ -204,35 +229,28 @@ classdef UserInterface < handle
         % Finds the closest control point when mouse is clicked
         function [M, Ind] = closestPoint(obj)
             
-            % 2x3 matlab matrix where mouse click is
+            % 2x3 matrix where mouse click is
             currpoint = obj.ax.CurrentPoint;
             % x and y coordinates of the mouse click
             coord = [currpoint(1,1); currpoint(1,2)];
 
+            % Distance between mouse click and each of the points
             d = obj.cp - coord;
             dnorm = sum(d.^2,1);
 
+            % Returns the index of the point closest to the mouse as well
+            % as the distance of this point from the mouse
             [M, Ind] = min(dnorm);
-% 
-%             if(dnorm < 1)
-%                 obj.mouseflag = true;
-%                 [~, Ind] = min(dnorm);
-%             
-%             else
-%                 obj.mouseflag = false;
-%                 Ind = -1;
-%             end
 
         end
         
-
         % Updates and draws control points
         function drawPoints(obj)
             obj.pointpatch.Vertices = obj.cp';
             obj.pointpatch.Faces = [1:size(obj.cp,2)]';
-            %axis tight equal;
         end
 
+        % Draws spline from the control poins
         function drawSpline(obj)
             obj.lines.XData =  obj.cp(1,:);
             obj.lines.YData = obj.cp(2,:);
@@ -241,37 +259,39 @@ classdef UserInterface < handle
             gamma = obj.spline.evaluate(t_samples);
             obj.splineplot.XData = gamma(1,:);
             obj.splineplot.YData = gamma(2,:);
-            %axis tight equal;
-
         end
 
+        % Updates spline information
         function updateSpline(obj, cp)
             obj.spline.setControlPoints(cp);
             obj.cp = cp;
-
         end
-
+        
+        % Draws the corresponding elastic strip
         function drawStrip(obj)
-
+            % Sample the curve, and approximate the arc-length parameter s at every
+            % sample
             t_samples = linspace(0,obj.spline.t_max,obj.num_samples);
             gamma = obj.spline.evaluate(t_samples);
             kappa = obj.spline.curvature(t_samples);
             to = gamma(:,2:end)-gamma(:,1:end-1);
             seg_lens = sqrt(sum(to.^2,1));
             s = [0 cumsum(seg_lens)];
-
+            
+            % Initialize stiffness optimizer
             [~, gamma_infl] = obj.spline.findInflectionPoints();
             lpopt = LPStiffnessOptimizer(gamma, kappa, gamma_infl);
-            %err = lpopt.err;
-            %if(err == 0)
+            
+            % Solve the linear programme to find K
             K = lpopt.optimizeWithInflections();
+
             if (lpopt.err == 0)
+                % Generate simple strip outline and plot
                 max_width = 0.05;
                 half_width = 0.5*max_width/max(K)*K;
                 p_simple = GeometryGenerator.generateProfileOutlineLoop(s,half_width,0.01);
                 SvgTools.exportCurves('spline-curve/strip_simple.svg', {p_simple}, 1e3/0.352778);
     
-                %obj.figstrip = patch(obj.axstrip);
                 obj.strip_patch.Vertices = p_simple';
                 obj.strip_patch.Faces = [1:size(p_simple,2)-1; 2:size(p_simple,2)]';
                 
@@ -280,7 +300,7 @@ classdef UserInterface < handle
                 axis tight equal;
 
             elseif(lpopt.err == 1)
-                
+                % Do not plot anything if the curve is infeasible
                 obj.strip_patch.Vertices = [];
                 obj.strip_patch.Faces = [];
                 
@@ -288,18 +308,10 @@ classdef UserInterface < handle
                 title('\color{red}Curve infeasible');
                 return;  
             end
-% 
-%             if isempty(K)
-%                 fprintf('Curve is infeasible!');
-%                 return;
-%             end
-
-
 
         end
 
-       
-
     end
+
 end
 
